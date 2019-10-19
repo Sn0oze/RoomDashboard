5c8387b2578e2bb4377ea037ebcd15f306998bb6
Jan <janewaldable@gmail.com>
2019-10-19 23:27:54 +0200

    Removed deleted ui imports. Added mat select.

--- dashboard-app/src/app/app.component.ts
+++ dashboard-app/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import buildingData from '../data/buildingData.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dashboard';
+  floor = null;
+
+  ngOnInit(): void {
+  }
+
+  onFloorSelected(floor): void {
+    if (floor) {
+      this.floor = buildingData[floor];
+    } else {
+      this.floor = null;
+    }
+  }
 }

feff3fecb7abec87abc1a89328e2c4f6b426e207
Jan <janewaldable@gmail.com>
2019-10-19 23:35:29 +0200

    Styled the ui and added simple logic that listens to floor selection changes as well as displays the selected floor's rooms in a mat-select and the selected room's job pipeline.

--- dashboard-app/src/app/ui/ui.component.ts
+++ dashboard-app/src/app/ui/ui.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnChanges, Input, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
   styleUrls: []
 })
-export class UiComponent implements OnInit {
+export class UiComponent implements OnInit, OnChanges {
+  @Input() floor: Object;
+
+  rooms: string[];
+  room: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    const newFloor = changes.floor.currentValue;
+    if (newFloor) {
+      console.log(Object.keys(newFloor));
+      this.rooms = Object.keys(newFloor);
+      this.room = this.rooms[0];
+
+    } else {
+      this.rooms = [];
+      this.room = '';
+    }
+  }
 }

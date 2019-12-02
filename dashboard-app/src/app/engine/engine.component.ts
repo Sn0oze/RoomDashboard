409a96b2fed75649af52510a84686b5da536e514
Jan <janewaldable@gmail.com>
2019-12-02 20:40:42 +0100

    changed layout to be a two column split between the floorpan and 3d model.

--- dashboard-app/src/app/engine/engine.component.ts
+++ dashboard-app/src/app/engine/engine.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, OnDestroy, ViewChild, Output, EventEmitter} from '@angular/core';
 import { EngineService } from './engine.service';
+import {FloorUserData} from '../core/models/floor-user-data.model';
 
 @Component({
   selector: 'app-engine',
@@ -9,13 +10,16 @@ export class EngineComponent implements OnInit, OnDestroy {
 
   @ViewChild('rendererCanvas', { static: true })
   private rendererCanvas: ElementRef<HTMLCanvasElement>;
-  @Output() floorSelected = new EventEmitter<string>();
+  @ViewChild('canvasContainer', { static: true})
+  private container: ElementRef<HTMLDivElement>;
+
+  @Output() floorSelected = new EventEmitter<FloorUserData>();
 
   constructor(
     private engine: EngineService) { }
 
   ngOnInit() {
-    this.engine.createScene(this.rendererCanvas);
+    this.engine.createScene(this.rendererCanvas, this.container);
     this.engine.animate();
     this.engine.floorClicked.subscribe(
       floor => {

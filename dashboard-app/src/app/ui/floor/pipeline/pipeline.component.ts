4bc2ace3780147242cae476d56cb0fb922314333
Jan <janewaldable@gmail.com>
2019-12-03 20:05:25 +0100

    added line to show when the current time in relation to timeline.

--- dashboard-app/src/app/ui/floor/pipeline/pipeline.component.ts
+++ dashboard-app/src/app/ui/floor/pipeline/pipeline.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit, Input} from '@angular/core';
 import {Job} from '../../../core/models/job.model';
 import * as moment from 'moment';
 import {TimeFormats} from '../../../core/constants/time-formats';
+import {Moment} from 'moment';
+import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pipeline',
@@ -12,14 +14,26 @@ export class PipelineComponent implements OnInit {
 
   @Input() job: Job;
   @Input() color = '#64C7CC';
+  private now = moment.utc().add(3, 'week').add(4, 'day');
+  private lineWidth = 4;
+  private start: Moment;
+  private end: Moment;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.start = moment.utc(this.job.start);
+    this.end = moment.utc(this.job.end);
   }
 
   formatTime(utcString): string {
     return moment.utc(utcString).format(TimeFormats.short);
+  }
+
+  progress(): SafeStyle {
+    const percentage = Math.floor((this.now.clone().diff(this.start) / this.end.clone().diff(this.start)) * 100);
+    const offset = percentage >= 50 ? this.lineWidth : 0;
+    return this.sanitizer.bypassSecurityTrustStyle(`calc(${percentage}% - ${offset}px)`);
   }
 
 }
